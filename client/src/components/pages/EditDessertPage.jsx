b64de1fa90ee23502272a002bb5c9aa7cd3ffeac
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2025-05-06 13:17:32 -0400

    Deleting dessert functionality

--- client/src/components/pages/EditDessertPage.jsx
+++ client/src/components/pages/EditDessertPage.jsx
@@ -1,11 +1,12 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import DessertForm from '../DessertForm';
-import { getDessert } from '../../actions';
+import { getDessert, deleteDessert } from '../../actions';
 import { useEffect, useState } from 'react';
 
 const EditDessertPage = () => {
   const [dessert, setDessert] = useState({});
   const { dessertId } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchDessert = async () => {
@@ -14,9 +15,16 @@ const EditDessertPage = () => {
     };
     fetchDessert();
   }, [dessertId]);
+
+  const handleDelete = async (e) => {
+    e.preventDefault();
+    await deleteDessert(dessertId);
+    navigate(`/categories/${dessert.category_id}/desserts`);
+  };
   return (
     <div>
       <DessertForm item={dessert} />
+      <button onClick={handleDelete}>delete</button>
     </div>
   );
 };
